20170f6b685d499e5a3eac01a2c035c2465cfc6b
Edws <eduardos.rocha17@gmail.com>
2020-05-10 22:11:07 -0300

    binario: Macro TRYFWRITE
    Criando macro TRYFWRITE para reduzir repetição de código

--- binario.c
+++ binario.c
@@ -1,8 +1,16 @@
 #include "binario.h"
 
+/**
+ * Tenta escrever qtde valores do tipo type a parte de ptr em file
+ * Se não der certo, desvia para o label fwrite_error
+ */
+#define TRYFWRITE(ptr, type, qtde, file) \
+    if (fwrite((ptr), sizeof(type), (qtde), (file)) != ((size_t)qtde)) goto fwrite_error
+
 Binario* binario_new(char* path) {
     if (!path) return NULL;
     Binario* binario = fopen(path, "wb");
+    int i;  // Iteradores
 
     char status = '1';
     int rrn = 0;
@@ -13,16 +21,15 @@ Binario* binario_new(char* path) {
     for (i = 0; i < 111; i++) lixo[i] = LIXO;
 
     // TODO sumir com o goto
-    if (fwrite(&status, sizeof(char), 1, binario) != 1) goto fwrite_erro;      // Status
-    if (fwrite(&rrn, sizeof(int), 1, binario) != 1) goto fwrite_erro;          // RRNProxRegistro
-    if (fwrite(&inseridos, sizeof(int), 1, binario) != 1) goto fwrite_erro;    // numeroRegistrosInseridos
-    if (fwrite(&removidos, sizeof(int), 1, binario) != 1) goto fwrite_erro;    // numeroRegistrosRemovidos
-    if (fwrite(&atualizados, sizeof(int), 1, binario) != 1) goto fwrite_erro;  // numeroRegistrosAtualizado
-    if (fwrite(lixo, sizeof(char), 111, binario) != 111) goto fwrite_erro;     // lixo (caracter '$')
-
+    TRYFWRITE(&status, char, 1, binario);
+    TRYFWRITE(&rrn, int, 1, binario);
+    TRYFWRITE(&inseridos, int, 1, binario);
+    TRYFWRITE(&removidos, int, 1, binario);
+    TRYFWRITE(&atualizados, int, 1, binario);
+    TRYFWRITE(lixo, char, 111, binario);
     return binario;
 
-fwrite_erro:
+fwrite_error:
     printf("Falha no carregamento do arquivo.");
     fclose(binario);
     return NULL;
@@ -49,11 +56,13 @@ bool binario_inserir(Binario* binario, Registro* registro) {
     if (!binario) return false;
 
     int i;  // Iteradores
+    char CHARNULO = '\0';
+    int tamDado;
 
     // Extraindo dados do registro
     char* cidadeMae = registro_getCidadeMae(registro);
-    int tamCidadeMae = strlen(cidadeMae);
     char* cidadeBebe = registro_getCidadeBebe(registro);
+    int tamCidadeMae = strlen(cidadeMae);
     int tamCidadeBebe = strlen(cidadeBebe);
     int idNascimento = registro_getIdNascimento(registro);
     int idadeMae = registro_getIdadeMae(registro);
@@ -70,24 +79,49 @@ bool binario_inserir(Binario* binario, Registro* registro) {
     // Quantidade de lixo no campo variável
     int qtdeLixo = 97 - tamCidadeBebe - tamCidadeMae;
 
-    if (fwrite(&tamCidadeMae, sizeof(int), 1, binario) != 1) return false;
-    if (fwrite(&tamCidadeBebe, sizeof(int), 1, binario) != 1) return false;
-    if (fwrite(cidadeMae, sizeof(char), tamCidadeMae, binario) != tamCidadeMae) return false;
-    if (fwrite(cidadeBebe, sizeof(char), tamCidadeBebe, binario) != tamCidadeBebe) return false;
-    if (fwrite(lixo, sizeof(char), qtdeLixo, binario) != qtdeLixo) return false;
-    if (fwrite(&idNascimento, sizeof(int), 1, binario) != 1) return false;
-    if (fwrite(&idadeMae, sizeof(int), 1, binario) != 1) return false;
-    if (strlen(dataNascimento) != 0) {
-        if (fwrite(dataNascimento, sizeof(char), 1, binario) != 1) return false;
-    } else {
-        if (fwrite(dataNascimento, sizeof(char), 1, binario)) return false;
-        if (fwrite(lixo, sizeof(char), 1, binario)) return false;
+    TRYFWRITE(&tamCidadeMae, int, 1, binario);
+    TRYFWRITE(&tamCidadeBebe, int, 1, binario);
+    TRYFWRITE(cidadeMae, char, tamCidadeMae, binario);
+    TRYFWRITE(cidadeBebe, int, tamCidadeBebe, binario);
+    TRYFWRITE(lixo, char, qtdeLixo, binario);
+    TRYFWRITE(&idNascimento, int, 1, binario);
+    TRYFWRITE(&idadeMae, int, 1, binario);
+
+    tamDado = strlen(dataNascimento);
+    TRYFWRITE(dataNascimento, char, tamDado, binario);
+    if (tamDado < 10) {
+        TRYFWRITE(&CHARNULO, char, 1, binario);
+        qtdeLixo = 10 - tamDado - 1;
+        TRYFWRITE(lixo, char, qtdeLixo, binario);
     }
-    if (fwrite(sexoBebe, sizeof(char), 1, binario) != 1) return false;
-    // TODO terminar
+
+    TRYFWRITE(&sexoBebe, char, 1, binario);
+
+    tamDado = strlen(estadoMae);
+    TRYFWRITE(estadoMae, char, tamDado, binario);
+    if (tamDado < 2) {
+        TRYFWRITE(&CHARNULO, char, 1, binario);
+        qtdeLixo = 2 - tamDado - 1;
+        TRYFWRITE(lixo, char, qtdeLixo, binario);
+    }
+
+    tamDado = strlen(estadoMae);
+    TRYFWRITE(estadoBebe, char, tamDado, binario);
+    if (tamDado < 2) {
+        TRYFWRITE(&CHARNULO, char, 1, binario);
+        qtdeLixo = 2 - tamDado - 1;
+        TRYFWRITE(lixo, char, qtdeLixo, binario);
+    }
+
+    return true;
+
+fwrite_error:
+    return false;
 }
 
 Registro* binario_leRegistro(Binario* binario) {
+    binario;
+    return registro_newVazio();
 }
 
 bool binario_atualizaCabecalho(char* path,
@@ -100,31 +134,31 @@ bool binario_atualizaCabecalho(char* path,
 
     if (status) {
         char aux = (status ? '1' : '0');
-        if (fwrite(&aux, sizeof(aux), 1, binario) != 1) goto fwrite_erro;
+        TRYFWRITE(&aux, char, 1, binario);
     } else {
         fseek(binario, sizeof(char), SEEK_CUR);
     }
 
     if (rrn) {
-        if (fwrite(rrn, sizeof(int), 1, binario) != 1) goto fwrite_erro;
+        TRYFWRITE(rrn, int, 1, binario);
     } else {
         fseek(binario, sizeof(int), SEEK_CUR);
     }
 
     if (inseridos) {
-        if (fwrite(inseridos, sizeof(int), 1, binario) != 1) goto fwrite_erro;
+        TRYFWRITE(inseridos, int, 1, binario);
     } else {
         fseek(binario, sizeof(int), SEEK_CUR);
     }
 
     if (removidos) {
-        if (fwrite(removidos, sizeof(int), 1, binario) != 1) goto fwrite_erro;
+        TRYFWRITE(removidos, int, 1, binario);
     } else {
         fseek(binario, sizeof(int), SEEK_CUR);
     }
 
     if (atualizados) {
-        if (fwrite(atualizados, sizeof(int), 1, binario) != 1) goto fwrite_erro;
+        TRYFWRITE(atualizados, int, 1, binario);
     } else {
         fseek(binario, sizeof(int), SEEK_CUR);
     }
@@ -133,7 +167,7 @@ bool binario_atualizaCabecalho(char* path,
     return true;
 
     // TODO sumir com o goto
-fwrite_erro:
+fwrite_error:
     fclose(binario);
     return false;
 }
