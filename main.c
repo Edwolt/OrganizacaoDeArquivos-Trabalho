179a8da144a2c1054ee7dd27a494b3235803f5a8
Edws <eduardos.rocha17@gmail.com>
2020-05-25 12:17:12 -0300

    main: Documentação: Novas opções
    Documentando novas opções que serão implementadas (de 3 a 7) criando um descrição sobre como ela deve se comporta
    
    Também foi criado uma descrição para as opções antigas

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include "registro.h"
 
 /**
- * Le um arquivo csv com nome src
+ * Le um arquivo CSV com nome src
  * e transforma em um arquivo binario e salva com nome dest
  */
 void opcao1(char* src, char* dest) {
@@ -88,14 +88,107 @@ int main() {
 
     while (scanf("%d", &opcao) == 1) {  // Enquanto houver entradas no stdin
         switch (opcao) {
+            /**
+             * 1 src dest
+             * src: nome do arquivo CSV de origem
+             * dest: nome do arquivo binario de destino
+             * 
+             * Cria arquivo binario dest com os dados do arquivo CSV src
+             */
             case 1:
                 scanf(" %s %s", src, dest);
                 opcao1(src, dest);
                 break;
 
+            /** 
+             * 2 src
+             * src: nome do arquivo binario
+             * 
+             * Imprime os dados de um arquivo binario de registros
+             */
             case 2:
                 scanf(" %s", src);
                 opcao2(src);
+                break;
+
+            /**
+             * 3 src m campo1 valor1 [campo2 valor2 [ ... ]]
+             * src: nome do arquivo binario
+             * m: numero de duplas campo-valor
+             * campo valor: campo (string) seguido de um valor (depende do campo)
+             * 
+             * Faz uma busca paramentrizada pelo arquivo buscando registros
+             * que cada campo_i tenha o valor_i 
+             */
+            case 3:
+                printf("Operação não implementada :(\n");
+                break;
+
+            /**
+             * 4 src rrn
+             * src: nome do arquivo binario
+             * rrn: RRN do regitro a ser recuperado
+             * 
+             * Imprime o valor do regitro com RRN igual ao passado
+             */
+            case 4:
+                printf("Operação não implementada :(\n");
+                break;
+
+            /**
+             * 5 src n
+             * m1 nome11 valor11 [nome12 valor12 [ ... ]]
+             * m2 nome21 valor21 [nome22 valor22 [ ... ]]
+             * ...
+             * mn nomen1 valorn1 [nomen2 valorn2 [ ... ]]
+             * 
+             * src: nome do arquivo binario
+             * n: numero de registros
+             * m: numero de dupla campo-valor
+             * 
+             * Remove registros seguindo n criterios diferentes
+             * Cada linha subsequente diz o criterio de remoção
+             * m_j diz quantas duplas campo-valor tem naquela linha
+             * Para um registro passar pelo críterio o campo_ij deve ter valor_ij
+             */
+            case 5:
+                printf("Operação não implementada :(\n");
+                break;
+
+            /**
+             * 6 src n
+             * cidadeMae1 cidadeBebe1 idNascimento1 idadeMae1 dataNascimento1 sexoBebe1 estadoMae1 estadoBebe1
+             * cidadeMae2 cidadeBebe2 idNascimento2 idadeMae2 dataNascimento2 sexoBebe2 estadoMae2 estadoBebe2
+             * ...
+             * cidadeMaen cidadeBeben idNascimenton idadeMaen dataNascimenton sexoBeben estadoMaen estadoBeben
+             * 
+             * src: nome do arquivo binario
+             * n: numero de registro
+             * 
+             * Insere n registros no arquivo binario src
+             * embaixo segue o dados de cada registro
+             */
+            case 6:
+                printf("Operação não implementada :(\n");
+                break;
+
+            /**
+             * 7 src n
+             * rrn1 m1 campo11 valor11 [nome12 valor12 [ ... ]]
+             * rrn2 m2 nome21 valor21 [nome22 valor22 [ ... ]]
+             * ...
+             * rrn3 mn nomen1 valorn1 [nomen2 valorn2 [ ... ]]
+             * 
+             * src: nome do arquivo binario
+             * n: numero de registro
+             * rrn: RRN do registro
+             * m: numero de duplas campo-valor
+             * 
+             * Atualiza n registros de rrn_i mudando o valor de m_i campos,
+             * fazendo que o campo_ij tenha valor_ij
+             */
+            case 7:
+                printf("Operação não implementada :(\n");
                 break;
 
             default:
