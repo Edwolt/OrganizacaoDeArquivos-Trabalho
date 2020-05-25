c41873cdffa28cd9f1e122e37c153bad34e4b054
Edws <eduardos.rocha17@gmail.com>
2020-05-25 14:34:20 -0300

    main: opcoes
    Colocando cada opção em uma função para que a main fique mais limpa
    Movendo documentação da opcao do switch para a função
    A partir de agora a função é responsável por ler do stdin

--- main.c
+++ main.c
@@ -7,11 +7,23 @@
 #include "csv.h"
 #include "registro.h"
 
+#define PATH_TAM 100  // Tamanho do nome do arquivo
+
 /**
- * Le um arquivo CSV com nome src
- * e transforma em um arquivo binario e salva com nome dest
+ * 1 src dest
+ * 
+ * src: arquivo csv
+ * dest:arquivo binario de registros
+ * 
+ * Le o arquivo CSV com nome src
+ * e armazena os dados lidos em um arquivo binario de registros,
+ * salvando-o com o nome dest
  */
-void opcao1(char* src, char* dest) {
+opcao1() {
+    char src[PATH_TAM];
+    char dest[PATH_TAM];
+    scanf(" %s %s", src, dest);
+
     FILE* csv = csv_abrir(src);
     if (!csv) {
         printf("Falha no carregamento do arquivo.\n");
@@ -29,10 +41,17 @@ void opcao1(char* src, char* dest) {
     csv_fechar(&csv);
 }
 
-/**
- * Imprime registros de um arquivo binario com nome path
+/** 
+ * 2 src
+ * 
+ * src: arquivo binario de registros
+ * 
+ * Imprime os dados do arquivo binario com nome path
  */
-void opcao2(char* path) {
+void opcao2() {
+    char path[PATH_TAM];
+    scanf(" %s", path);
+
     int i;  // Iteradores
 
     bool status;
@@ -81,122 +100,126 @@ void opcao2(char* path) {
     binario_fechar(&bin);
 }
 
-int main() {
-    char arq1[100];
-    char arq2[100];
-    int opcao;
+/**
+ * 3 bin m campo[1] valor[1] ... campo[m] valor[m]
+ * 
+ * bin: arquivo binario de registros
+ * m: numero de duplas campo-valor que tem na linha
+ * campo[i]: string
+ * valor[i]: depende do campo[i]
+ * 
+ * Faz uma busca paramentrizada pelo arquivo bin
+ * que cada campo[i] tenha o valor[i]
+ */
+void opcao3() {
+    printf("Operação não implementada :(\n");
+}
 
+/**
+ * 4 bin rrn
+ * 
+ * bin: nome do arquivo binario
+ * rrn: RRN do regitro a ser recuperado
+ * 
+ * Imprime o valor do regitro de RRN igual a rrn do arquivo binario bin
+ */
+void opcao4() {
+    printf("Operação não implementada :(\n");
+}
+
+/**
+ * 5 bin n
+ * m[1] campo[1][1] valor[1][1] ... campo[1][m[1]] valor[1][m[1]]
+ * m[2] campo[2][1] valor[2][1] ... campo[2][m[2]] valor[2][m[2]]
+ * ...
+ * m[n] campo[n][1] valor[n][1] ... campo[n][m[n]] valor[n][m[n]]
+ * 
+ * bin: arquivo binario de registros
+ * n: numero de linhas abaixo
+ * m[i]: numero de duplas campo valor que tem na linha
+ * campo[i][j]: string
+ * valor[i][j]: depende do campo[i][j]
+ * 
+ * Remove registros seguindo n criterios diferentes do arquivo binario bin
+ * Cada linha subsequente diz o criterio de remoção
+ * Para um registro passar pelo criterio e ser removido
+ * No arquivo src o campo[i][j] deve valer valor[i][j]
+ */
+void opcao5() {
+    printf("Operação não implementada :(\n");
+}
+
+/**
+ * 6 bin n
+ * dados[1]
+ * dados[2]
+ * ...
+ * dados[n]
+ * 
+ * bin: nome do arquivo binario
+ * n: numero de linhas abaixo
+ * dados[i]: composto por valores de cada campo do registro, deve ficar assim:
+ * cidadeMae[i] cidadeBebe[i] idNascimento[i] idadeMae[i] dataNascimento[i] sexoBebe[i] estadoMae[i] estadoBebe[i]
+ * 
+ * Insere n registros no arquivo binario bin
+ * O conteudo de cada registro inserido eh dados[i]
+ */
+void opcao6() {
+    printf("Operação não implementada :(\n");
+}
+
+/**
+ * 7 bin n
+ * rrn[1] m[1] campo[1][1] valor[1][1] ... campo[1][m[1]] valor[1][m[1]]
+ * rrn[2] m[2] campo[2][1] valor[2][1] ... campo[2][m[2]] valor[1][m[2]]
+ * ...
+ * rrn[n] m[n] campo[n][1] valor[n][1] ... campo[n][m[n]] valor[n][m[n]]
+ * 
+ * bin: arquivo binario
+ * n: numero de linhas abaixo
+ * rrn[i]: inteiro
+ * m[i]: numero de duplas campo-valor
+ * campo[i][j]: string
+ * valor[i][j]: depende do campo[i][j]
+ * 
+ * Atualiza n registros do arquivo binario bin
+ * O registro de valor rrn[i] passa a tem o valor dos seus campos mudados
+ * sendo que o campo[i][j] passa a valer valor[i][j]
+ */
+void opcao7() {
+    printf("Operação não implementada :(\n");
+}
+
+int main() {
+    int opcao;
     while (scanf("%d", &opcao) == 1) {  // Enquanto houver entradas no stdin
         switch (opcao) {
-            /**
-             * 1 src dest
-             * src: arquivo csv
-             * dest:arquivo binario de registros
-             * 
-             * Cria arquivo binario dest com os dados do arquivo CSV src
-             */
             case 1:
-                scanf(" %s %s", arq1, arq2);
-                opcao1(arq1, arq2);
+                opcao1();
                 break;
 
-            /** 
-             * 2 src
-             * src: arquivo binario de registros
-             * 
-             * Imprime os dados do arquivo binario src
-             */
             case 2:
-                scanf(" %s", arq1);
-                opcao2(arq1);
+                opcao2();
                 break;
 
-            /**
-             * 3 bin m campo[1] valor[1] ... campo[m] valor[m]
-             * bin: arquivo binario de registros
-             * m: numero de duplas campo-valor que tem na linha
-             * campo[i]: string
-             * valor[i]: depende do campo[i]
-             * 
-             * Faz uma busca paramentrizada pelo arquivo bin
-             * que cada campo[i] tenha o valor[i]
-             */
             case 3:
-                printf("Operação não implementada :(\n");
+                opcao3();
                 break;
 
-            /**
-             * 4 bin rrn
-             * bin: nome do arquivo binario
-             * rrn: RRN do regitro a ser recuperado
-             * 
-             * Imprime o valor do regitro de RRN igual a rrn do arquivo binario bin
-             */
             case 4:
-                printf("Operação não implementada :(\n");
+                opcao4();
                 break;
 
-            /**
-             * 5 bin n
-             * m[1] campo[1][1] valor[1][1] ... campo[1][m[1]] valor[1][m[1]]
-             * m[2] campo[2][1] valor[2][1] ... campo[2][m[2]] valor[2][m[2]]
-             * ...
-             * m[n] campo[n][1] valor[n][1] ... campo[n][m[n]] valor[n][m[n]]
-             * 
-             * bin: arquivo binario de registros
-             * n: numero de linhas abaixo
-             * m[i]: numero de duplas campo valor que tem na linha
-             * campo[i][j]: string
-             * valor[i][j]: depende do campo[i][j]
-             * 
-             * Remove registros seguindo n criterios diferentes do arquivo binario bin
-             * Cada linha subsequente diz o criterio de remoção
-             * Para um registro passar pelo criterio e ser removido
-             * No arquivo src o campo[i][j] deve valer valor[i][j]
-             */
             case 5:
-                printf("Operação não implementada :(\n");
+                opcao5();
                 break;
 
-            /**
-             * 6 bin n
-             * dados[1]
-             * dados[2]
-             * ...
-             * dados[n]
-             * 
-             * bin: nome do arquivo binario
-             * n: numero de linhas abaixo
-             * dados[i]: composto por valores de cada campo do registro, deve ficar assim:
-             * cidadeMae[i] cidadeBebe[i] idNascimento[i] idadeMae[i] dataNascimento[i] sexoBebe[i] estadoMae[i] estadoBebe[i]
-             * 
-             * Insere n registros no arquivo binario bin
-             * O conteudo de cada registro inserido eh dados[i]
-             */
             case 6:
-                printf("Operação não implementada :(\n");
+                opcao6();
                 break;
 
-            /**
-             * 7 bin n
-             * rrn[1] m[1] campo[1][1] valor[1][1] ... campo[1][m[1]] valor[1][m[1]]
-             * rrn[2] m[2] campo[2][1] valor[2][1] ... campo[2][m[2]] valor[1][m[2]]
-             * ...
-             * rrn[n] m[n] campo[n][1] valor[n][1] ... campo[n][m[n]] valor[n][m[n]]
-             * 
-             * bin: arquivo binario
-             * n: numero de linhas abaixo
-             * rrn[i]: inteiro
-             * m[i]: numero de duplas campo-valor
-             * campo[i][j]: string
-             * valor[i][j]: depende do campo[i][j]
-             * 
-             * Atualiza n registros do arquivo binario bin
-             * O registro de valor rrn[i] passa a tem o valor dos seus campos mudados
-             * sendo que o campo[i][j] passa a valer valor[i][j]
-             */
             case 7:
-                printf("Operação não implementada :(\n");
+                opcao7();
                 break;
 
             default:
