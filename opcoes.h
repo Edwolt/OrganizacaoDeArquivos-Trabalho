e72e7adb896870bfb218017e4f85f61c3630b587
Edws <eduardos.rocha17@gmail.com>
2020-05-26 18:19:46 -0300

    opcoes
    Separando opções da main para deixa ela mais limpa. O código ficou no arquivo opcoes

---
+++ opcoes.h
@@ -0,0 +1,17 @@
+#ifndef OPCOES_H
+#define OPCOES_H
+
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "binario.h"
+#include "binarioNaTela.h"
+#include "csv.h"
+#include "registro.h"
+
+/**
+ * Executa a instrucao de numero n
+ */
+bool opcoes_executar(int n);
+
+#endif
