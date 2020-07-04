5848b7f6a3283b57400f5c41eeaa9051807ed80f
Edws <eduardos.rocha17@gmail.com>
2020-07-04 18:00:49 -0300

    opcoes: Documentação

--- src/opcoes.h
+++ src/opcoes.h
@@ -13,9 +13,10 @@
 /**
  * Executa a instrucao de numero n
  * Considera que so foi lido o codigo da operacao
- * e que em seguida estao os dados para execucao
+ * e que em seguida estao os dados necessarios para execucao
  * 
- * Retorna se foi possivel executar essa opcao
+ * Retorna se foi possivel ou nao executar essa opcao
+ * (Executar a opcao eh diferente de ocorrer um erro durante a execução)
  */
 bool opcoes_executar(int n);
 
