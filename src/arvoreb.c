d83daed6519f766028ea56b4748308a53f48154c
Edws <eduardos.rocha17@gmail.com>
2020-07-08 15:19:16 -0300

    arvoreb
    Iniciando o desenvolvimento do módulo de arvore B

---
+++ src/arvoreb.c
@@ -0,0 +1,10 @@
+#include "arvoreb.h"
+
+#define CHAVES_NUM 2
+
+struct Pagina {
+    int nivel;
+    int n;
+    int chaves[CHAVES_NUM];
+    int ponteiro[CHAVES_NUM - 1];
+};
---
+++ src/arvoreb.h
@@ -0,0 +1,12 @@
+#ifndef ARVORE_B_H
+#define ARVORE_B_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Indice Indice;
+typedef struct Pagina Pagina;
+
+Indice *indice_criar(char *path);
+
+#endif
