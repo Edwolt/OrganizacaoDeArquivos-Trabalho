a1ff0aaade94ba53a45187016ca10bac54d86862
Edws <eduardos.rocha17@gmail.com>
2020-07-10 12:07:10 -0300

    indice
    Iniciando criação de indice

--- src/indice.c
+++ src/indice.c
@@ -2,9 +2,74 @@
 
 #define CHAVES_NUM 2
 
+typedef struct Pagina Pagina;
+
+struct Indice {
+    FILE* file;
+    char* path;
+    const char* modes;
+    Pagina* raiz;
+
+    // Cabecalho
+    char status;
+    int noRaiz;
+    int nroNiveis;
+    int proxRRN;
+    int nroChaves;
+};
+
 struct Pagina {
     int nivel;
     int n;
     int chaves[CHAVES_NUM];
     int ponteiro[CHAVES_NUM - 1];
 };
+
+//* ====================== *//
+//* ===== Constantes ===== *//
+//* ====================== *//
+
+#define TAM_REG 72
+#define TAM_LIXOC 55
+
+static const char LIXO[TAM_REG + 1] = "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$";
+
+//* ============================ *//
+//* ===== Métodos Privados ===== *//
+//* ============================ *//
+
+/**
+ * Abri um arquivo e salva em indice
+ * Se já tiver um arquivo aberto fecha ele
+ */
+inline static void abrirArquivo(Indice* binario) {
+    if (binario->file) fclose(binario->file);
+    binario->file = fopen(binario->path, binario->modes);
+}
+
+//* ============================ *//
+//* ===== Métodos Publicos ===== *//
+//* ============================ *//
+
+Indice* indice_criar(char* path) {
+    Indice* indice = malloc(sizeof(Indice));
+    if (!indice) return NULL;
+
+    indice->file = NULL;
+    indice->path = path;
+    indice->modes = "wb";
+
+    abrirArquivo(indice);
+    if (!indice->file) {
+        free(indice);
+        return NULL;
+    }
+
+    indice->status = '0';
+    indice->noRaiz = 0;
+    indice->nroNiveis = 0;
+    indice->proxRRN = 0;
+    indice->nroChaves = 0;
+
+    return NULL;
+}
--- src/indice.h
+++ src/indice.h
@@ -1,12 +1,33 @@
 #ifndef INDICE_H
 #define INDICE_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>  // Arquivos
+#include <stdlib.h>  // Alocao Dinamica
 
+#include "registro.h"
+#include "util.h"
 
+/**
+ * TAD Indices para arquivo binario
+ * Implementa um arquivo de indices com Arvore B
+ */
 typedef struct Indice Indice;
 
-Indice *indice_criar(char *path);
+/**
+ * Cria um arquivo de indice vazio para escrita com nome path
+ * No arquivo ja eh escrito o registro cabecalho marcando o arquivo como inconsistente
+ * O arquivo aponta para o primeiro ?
+ * 
+ * Se o arquivo ja existir, sobrescreve ele com um arquivo novo
+ * Retorna NULL se nao for possivel criar o arquivo
+ */
+Indice* indice_criar(char* path);
+
+Indice* indice_abrirLeitura(char* path);
+
+Indice* indice_abrirEscrita(char* path);
+
+Registro* indice_buscar(char* path);
 
 #endif
